d11d193441940705a9fdd1f3bb3aa9dd53c5a1c1
resueman <ukatrichko@gmail.com>
2025-02-13 20:17:44 +0300

    Remove TODO comment

--- internal/entity/operation.go
+++ internal/entity/operation.go
@@ -2,13 +2,13 @@ package entity
 
 type PurchaseOperation struct {
 	ItemID            int `db:"item_id"`
-	CustomerAccountID int `db:"customer_account_id"` // TODO: -> customerUsername
+	CustomerAccountID int `db:"customer_account_id"`
 	Quantity          int `db:"quantity"`
 	TotalPrice        int `db:"total_price"`
 }
 
 type TransferOperation struct {
-	SenderAccountID    int `db:"sender_account_id"`    // TODO: -> senderUsername
-	RecipientAccountID int `db:"recipient_account_id"` // TODO: -> recipientUsername
+	SenderAccountID    int `db:"sender_account_id"`
+	RecipientAccountID int `db:"recipient_account_id"`
 	Amount             int `db:"amount"`
 }
